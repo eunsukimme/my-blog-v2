64f9a7a146b42ab5fc8b1cfc41fd2359c0a944f5
eunsukimme <eunsu.dev@gmail.com>
2022-01-21 23:29:47 +0900

    feat: edit siteMetadata and .env

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,33 +1,33 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'Undefined - Blog by Eunsu Kim',
+  author: 'Eunsu Kim',
+  headerTitle: 'Undefined - Blog by Eunsu Kim',
+  description: 'A Blog by Eunsu Kim',
+  language: 'ko-kr',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app', // TODO:
+  siteRepo: 'https://github.com/eunsukimme/my-blog-v2',
+  siteLogo: '/static/images/logo.png', // TODO:
+  image: '/static/images/avatar.png', // TODO:
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  email: 'eunsu.dev@gmail.com',
+  github: 'https://github.com/eunsukimme',
+  twitter: 'https://twitter.com/EunsuKim17',
+  facebook: 'https://www.facebook.com/people/%EA%B9%80%EC%9D%80%EC%88%98/100003647254099/',
+  // youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/eunsukimme/',
+  locale: 'ko-KR',
   analytics: {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    // plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // simpleAnalytics: false, // true or false
+    // umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: 'G-QBRFDG7Y7P', // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    // provider: 'buttondown',
   },
   comment: {
     // Select a provider and use the environment variables associated to it
@@ -40,13 +40,13 @@ const siteMetadata = {
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
+      mapping: 'title', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: 'preferred_color_scheme',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
@@ -54,22 +54,22 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
-    utterancesConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://utteranc.es/
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
-      // theme example: github-light, github-dark, preferred-color-scheme
-      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-      theme: '',
-      // theme when dark mode
-      darkTheme: '',
-    },
-    disqusConfig: {
-      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
-    },
+    // utterancesConfig: {
+    //   // Visit the link below, and follow the steps in the 'configuration' section
+    //   // https://utteranc.es/
+    //   repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+    //   issueTerm: '', // supported options: pathname, url, title
+    //   label: '', // label (optional): Comment 💬
+    //   // theme example: github-light, github-dark, preferred-color-scheme
+    //   // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+    //   theme: '',
+    //   // theme when dark mode
+    //   darkTheme: '',
+    // },
+    // disqusConfig: {
+    //   // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+    //   shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+    // },
   },
 }
 
