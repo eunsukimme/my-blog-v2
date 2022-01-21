62357d620ea0743dcd32136bf7f80c2f827993b0
eunsukimme <eunsu.dev@gmail.com>
2022-01-21 23:53:54 +0900

    feat: edit project info

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,9 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '롤링페이퍼',
+    description: `코로나로 인해 지인들과 만날 기회가 줄어들자 마음을 전하는 것도 좀 처럼 쉽지 않은 요즘, 롤링페이퍼로 따뜻한 마음을 전달할 수 있고자 하는 비전으로 서비스를 만들고 있습니다.`,
+    imgSrc: '/static/images/rollingpaper_logo.png',
+    href: 'https://rollingpaper.site/',
   },
 ]
 
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            제가 참여한 프로젝트를 소개합니다
           </p>
         </div>
         <div className="container py-12">
