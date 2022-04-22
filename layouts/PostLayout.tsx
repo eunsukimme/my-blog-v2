4fcc9b94e10269f10198c812405246ee0e3aa37f
eunsukimme <eunsu.dev@gmail.com>
2022-04-23 00:33:54 +0900

    feat: edit back to blog link

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -153,7 +153,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
               </div>
               <div className="pt-4 xl:pt-8">
                 <Link
-                  href="/blog"
+                  href="/posts"
                   className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                 >
                   &larr; Back to the blog
